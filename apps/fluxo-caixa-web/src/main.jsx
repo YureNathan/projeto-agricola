5318369f094c0fa95498a4bcf318d11127cf79fe
Tadeu Almeida Rodriguez <tadeu.ar@aluno.ifsc.edu.br>
2026-09-17 12:07:58 -0300

    feat: marca campos obrigatorios

--- apps/fluxo-caixa-web/src/main.jsx
+++ apps/fluxo-caixa-web/src/main.jsx
@@ -3,14 +3,23 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
-import { ProvedorNotificacoes } from './componentes/Notificacoes.jsx'
+
+function marcarCamposObrigatorios() {
+    document
+        .querySelectorAll('input[required][id], select[required][id], textarea[required][id]')
+        .forEach((campo) => {
+            const rotulo = document.querySelector(`label[for="${campo.id}"]`)
+
+            if (rotulo) {
+                rotulo.classList.add('campo-obrigatorio')
+            }
+        })
+}
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <ProvedorNotificacoes>
-                <App />
-            </ProvedorNotificacoes>
+            <App />
         </BrowserRouter>
     </StrictMode>,
 )
@@ -20,3 +29,14 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js')
     })
 }
+
+marcarCamposObrigatorios()
+
+const observadorCamposObrigatorios = new MutationObserver(
+    marcarCamposObrigatorios,
+)
+
+observadorCamposObrigatorios.observe(document.body, {
+    childList: true,
+    subtree: true,
+})
