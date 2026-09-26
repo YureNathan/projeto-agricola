c8f8b2cca8fc968f013b62b276beedd81e6974b8
Tadeu Almeida Rodriguez <tadeu.ar@aluno.ifsc.edu.br>
2026-09-26 12:41:12 -0300

    fix: registra ObjectMapper para integracao Asaas

--- apps/fluxo-caixa-api/src/main/java/br/com/fluxocaixa/integration/asaas/AsaasConfig.java
+++ apps/fluxo-caixa-api/src/main/java/br/com/fluxocaixa/integration/asaas/AsaasConfig.java
@@ -1,9 +1,19 @@
 package br.com.fluxocaixa.integration.asaas;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(AsaasProperties.class)
 public class AsaasConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .findAndRegisterModules();
+    }
 }
